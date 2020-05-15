3efa097de2a3f9f741b53184ce608684c44a4c2f
Alexey <mr.wert2002@gmail.com>
2020-05-15 11:18:40 +0300

    refactor

--- src/main/java/org/moren/spring/controller/NoteApiController.java
+++ src/main/java/org/moren/spring/controller/NoteApiController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-public class NoteRestController {
+public class NoteApiController {
 
 	private NoteService NoteService;
 
