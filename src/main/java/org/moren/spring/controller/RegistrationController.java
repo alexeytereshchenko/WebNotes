dcac8d95f5ce2dc98e7b927acbfa475260d17b7a
Alexey <mr.wert2002@gmail.com>
2020-06-21 20:00:33 +0300

    add auto login

--- src/main/java/org/moren/spring/controller/RegistrationController.java
+++ src/main/java/org/moren/spring/controller/RegistrationController.java
@@ -1,5 +1,6 @@
 package org.moren.spring.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.moren.spring.model.User;
 import org.moren.spring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @Controller
+@Log4j2
 public class RegistrationController {
 
     private UserService userService;
@@ -27,7 +31,11 @@ public class RegistrationController {
     @PostMapping("/registration")
     public String addUser(@ModelAttribute("user") @Valid User user,
                           BindingResult bindingResult,
-                          Model model) {
+                          Model model,
+                          HttpServletRequest request) {
+
+        String username = user.getUsername();
+        String password = user.getPassword();
 
         if (bindingResult.hasErrors()) {
             return "registration";
@@ -37,6 +45,12 @@ public class RegistrationController {
         } else if (!userService.save(user)){
             model.addAttribute("usernameError", "A user with the same name already exists.");
             return "registration";
+        }
+
+        try {
+            request.login(username, password);
+        } catch (ServletException e) {
+            log.debug("Autologin fail", e);
         }
 
         return "redirect:/";
